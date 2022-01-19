bb13252a38be083ce6e0799e56e10a3b271d55b4
yuhui <yuhuiliaofan@gmail.com>
2022-01-19 23:26:31 +0100

    2e partie_V3

--- js/cards.js
+++ js/cards.js
@@ -1,20 +1,25 @@
 async function render() {
     const apiRestCountries = await fetch('https://restcountries.com/v3.1/region/europe?fields=name,capital,flags,maps');
     const dataListPaysFormJson = await apiRestCountries.json();
-    const number = new Intl.NumberFormat("en");
 
     let countryList = "";
     for (const itemElementItere of dataListPaysFormJson) {
         countryList +=
-            `<tr>
-            <th class="text-nowrap">${itemElementItere.name.official}</th>
-            <td class="text-end">${number.format(itemElementItere.area)}</td>
-            <td class="text-end">${number.format(itemElementItere.population)}</td>
-            <td class="text-start text-nowrap">${itemElementItere.capital[0]}</td>
-        </tr>`
+            `<div classe = "col"
+            <div classe = "card h-106">
+            >
+            </div>`
+        
+            // <img src="..." class="card-img-top" alt="...">
+            < div class="card-body" >
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div >
     }
 
-    document.querySelector("#table-body").innerHTML = countryList;
+    document.querySelector("#cardcountries").innerHTML = countryList;
     // const elementHTMLOuJeVeuxAfficherLesDonnees = 
 
     // elementHTMLOuJeVeuxAfficherLesDonnees.innerHTML = countryList;
